851740e2d6b34b4aeefb2b9aa924228d3f23e91d
fedeatanasoff <fede.atanasoff@gmail.com>
2018-12-03 16:19:42 -0300

    agregando el headr a la plantilla. agregando imagenes generales

---
+++ vistas/modulos/header.php
@@ -0,0 +1,39 @@
+<header class="main-header">
+    <!-- Logo -->
+    <a href="../../index2.html" class="logo">
+      <!-- mini logo for sidebar mini 50x50 pixels -->
+      <span class="logo-mini"><b>S</b>V</span>
+      <!-- logo for regular state and mobile devices -->
+      <span class="logo-lg"><b>Sistema</b>Ventas</span>
+    </a>
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top">
+      <!-- Sidebar toggle button-->
+      <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+        <span class="sr-only">Toggle navigation</span>
+        
+      </a>
+
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+          
+          <!-- User Account: style can be found in dropdown.less -->
+          <li class="dropdown user user-menu">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+              <img src="vistas/img/usuarios/default/anonymous.png" class="user-image" alt="User Image">
+              <span class="hidden-xs">Usuario Administrador</span>
+            </a>
+            <ul class="dropdown-menu">
+              <!-- User image -->
+              <li class="user-body">
+                <div class="pull-right">
+                    <a href="#" class="btn btn-default btn-flat">Salir</a>
+                </div>
+              </li>
+            </ul>
+          </li>
+          
+        </ul>
+      </div>
+    </nav>
+  </header>
