87ca45fd0613ac84f9db1c7cb9352d61fabd234b
fedeatanasoff <fede.atanasoff@gmail.com>
2018-12-04 00:13:26 -0300

    creando lista blanca de rutas. creando archivos para cada ruta con sus controlador y modelo(vacio)

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php
 
+// Controladores
 require_once "controladores/plantilla.controlador.php";
+require_once "controladores/usuarios.controlador.php";
+require_once "controladores/categorias.controlador.php";
+require_once "controladores/clientes.controlador.php";
+require_once "controladores/ventas.controlador.php";
+require_once "controladores/productos.controlador.php";
+
+// modelos
+require_once "controladores/usuarios.modelo.php";
+require_once "controladores/categorias.modelo.php";
+require_once "controladores/clientes.modelo.php";
+require_once "controladores/ventas.modelo.php";
+require_once "controladores/productos.modelo.php";
 
 $plantilla = new ControladorPlantilla();
-$plantilla -> ctrPlantilla();
+$plantilla->ctrPlantilla();
--- vistas/modulos/clientes.php
+++ vistas/modulos/clientes.php
@@ -3,13 +3,12 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        Blank page
-        <small>it all starts here</small>
+        Clientes
+        <small>Panel de Control</small>
       </h1>
       <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li><a href="#">Examples</a></li>
-        <li class="active">Blank page</li>
+        <li><a href="inicio"><i class="fa fa-dashboard"></i> Inicio</a></li>
+        <li><a href="clientes">Clientes</a></li>
       </ol>
     </section>
 
