bdc351616816c2507b40baf572460ac8a0e53f54
fedeatanasoff <fede.atanasoff@gmail.com>
2018-12-05 01:02:41 -0300

    empezando con el maquetado de usuario

--- vistas/modulos/usuarios.php
+++ vistas/modulos/usuarios.php
@@ -18,24 +18,60 @@
       <!-- Default box -->
       <div class="box">
         <div class="box-header with-border">
-          <h3 class="box-title">Title</h3>
-
-          <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
-                    title="Collapse">
-              <i class="fa fa-minus"></i></button>
-            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-              <i class="fa fa-times"></i></button>
-          </div>
+          <div class="btn btn-primary" data-toggle="modal" data-target="#modalAgregarUsuario">Agregar Usuario</div>
         </div>
         <div class="box-body">
-          Start creating your amazing application!
+        
+        <table class="table table-bordered table-striped">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Nombre</th>
+              <th>Usuarios</th>
+              <th>Fotos</th>
+              <th>Perfil</th>
+              <th>Estados</th>
+              <th>Ultimo Login</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+            <td>1</td>
+            <td>Usuario administrador</td>
+            <td>admin</td>
+            <td><img src="vistas/img/usuarios/default/anonymous.png" class="img-thumbnail" width="40px" alt=""></td>
+            <td>Administrador</td>
+            <td><button class="btn btn-success btn-xs">Activado</button></td>
+            <td>2017-12-11 12:33:32</td>
+            <td>
+              <div class="btn-group">
+                <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+              </div>
+            </td>
+            </tr>
+            <tr>
+            <td>2</td>
+            <td>Usuario Vendedpr</td>
+            <td>admin</td>
+            <td><img src="vistas/img/usuarios/default/anonymous.png" class="img-thumbnail" width="40px" alt=""></td>
+            <td>Administrador</td>
+            <td><button class="btn btn-danger btn-xs">Desactivado</button></td>
+            <td>2017-12-11 12:33:32</td>
+            <td>
+              <div class="btn-group">
+                <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+              </div>
+            </td>
+            </tr>
+          </tbody>
+        </table>
+          
         </div>
         <!-- /.box-body -->
-        <div class="box-footer">
-          Footer
-        </div>
-        <!-- /.box-footer-->
+        
       </div>
       <!-- /.box -->
 
@@ -43,3 +79,27 @@
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
+
+  <!-- Trigger the modal with a button -->
+<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+
+<!-- Modal -->
+<div id="modalAgregarUsuario" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Modal Header</h4>
+      </div>
+      <div class="modal-body">
+        <p>Some text in the modal.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
